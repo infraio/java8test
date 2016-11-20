8a9032c6f2f6541afc54833e8fcfe72ec34b568f
Guanghao Zhang <zghaobac@gmail.com>
2016-11-20 16:25:31 +0800

    add more test for flatMap

--- src/main/java/com/xiaohao/stream/TestFlatMap.java
+++ src/main/java/com/xiaohao/stream/TestFlatMap.java
@@ -1,17 +1,32 @@
 package com.xiaohao.stream;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class TestFlatMap {
 
   public static void main(String[] args) {
-    IntStream.range(1, 4)
-      .mapToObj(i -> new Foo("Foo " + i))
-      .peek(f -> IntStream.range(1, 4).mapToObj(i -> new Bar(f.name + " -> Bar " + i)).forEach(b -> f.bars.add(b)))
-      .flatMap(f -> f.bars.stream())
-      .forEach(b -> System.out.println(b.name));
+    IntStream.range(1, 4).mapToObj(i -> new Foo("Foo " + i))
+        .peek(f -> IntStream.range(1, 4).mapToObj(i -> new Bar(f.name + " -> Bar " + i))
+            .forEach(b -> f.bars.add(b)))
+        .flatMap(f -> f.bars.stream()).forEach(b -> System.out.println(b.name));
+
+    List<String> words = Arrays.asList("Hello", "FlatMap");
+    List<String> uniqueChars = words.stream().map(word -> word.split("")).flatMap(Arrays::stream)
+        .distinct().collect(Collectors.toList());
+    System.out.println(uniqueChars);
+
+    List<Integer> list1 = Arrays.asList(1, 2, 3);
+    List<Integer> list2 = Arrays.asList(4, 5);
+    List<int[]> pairs = list1.stream().flatMap(i -> list2.stream().map(j -> new int[] { i, j }))
+        .limit(3).collect(Collectors.toList());
+    pairs.stream().forEach(array -> {
+      Arrays.stream(array).forEach(i -> System.out.print(i + "\t"));
+      System.out.println();
+    });
   }
 
 }
